7e4b346ad829994b1d13585c1f0bf16bf0716172
Nico D. Alfonso <nicodalfonso@gmail.com>
2020-08-19 22:40:17 -0400

    Implemented bonus features
    Refactored code to account for batch loading
    
    Limits photo array mapping to the next batch only to remove redundancy

--- src/App.js
+++ src/App.js
@@ -10,13 +10,18 @@ const PHOTO_LIST_URL = "https://picsum.photos/list";
 class App extends Component {
     state = {
         photos: [],
+        visiblePhotos: [],
+        photosPerLoad: 10,
+        timesLoaded: 0,
     };
 
     componentDidMount = async () => {
         try {
             const photosArr = await (await fetch(PHOTO_LIST_URL)).json();
-            this.setState(() => ({
+            this.setState((state) => ({
                 photos: photosArr,
+                visiblePhotos: this.toJSX(photosArr, 0, state.photosPerLoad),
+                timesLoaded: state.timesLoaded + 1,
             }));
         } catch (err) {
             console.log(err);
@@ -24,22 +29,45 @@ class App extends Component {
         }
     };
 
+    toJSX = (photosArr, firstPhoto, lastPhoto) => {
+        return photosArr
+            .slice(firstPhoto, lastPhoto)
+            .map((photo) => (
+                <img
+                    alt={photo.filename}
+                    key={photo.id}
+                    src={PHOTO_URL(photo.id)}
+                />
+            ));
+    };
+
+    loadMorePhotos = (nextBatch) => {
+        this.setState((state) => ({
+            visiblePhotos: state.visiblePhotos.concat(nextBatch),
+            timesLoaded: state.timesLoaded + 1,
+        }));
+    };
+
     render() {
-        const { photos = [] } = this.state;
+        const {
+            photos = [],
+            visiblePhotos: visiblePhotos = [],
+            photosPerLoad,
+            timesLoaded,
+        } = this.state;
+        const firstPhoto = timesLoaded * photosPerLoad;
+        const lastPhoto = (timesLoaded + 1) * photosPerLoad;
+        const nextBatch = this.toJSX(photos, firstPhoto, lastPhoto);
+
         return (
             <React.Fragment>
                 <header>
                     <h1>Photo Wall</h1>
                 </header>
-                <div className="collage">
-                    {photos.map((photo) => (
-                        <img
-                            alt={photo.filename}
-                            key={photo.id}
-                            src={PHOTO_URL(photo.id)}
-                        />
-                    ))}
-                </div>
+                <div className="collage">{visiblePhotos}</div>
+                <button onClick={() => this.loadMorePhotos(nextBatch)}>
+                    Show me what you got
+                </button>
             </React.Fragment>
         );
     }
