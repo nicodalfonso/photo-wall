f343ea722ab660d6be8677aaec31db555209e938
Nico D. Alfonso <nicodalfonso@gmail.com>
2020-08-19 21:46:46 -0400

    MVP

--- src/App.js
+++ src/App.js
@@ -2,47 +2,47 @@ import React, { Component } from "react";
 import "./App.css";
 
 // This URL can be combined with an photo id to fetch an photo.
-const PHOTO_URL = photoId => `https://picsum.photos/id/${photoId}/200/200`;
+const PHOTO_URL = (photoId) => `https://picsum.photos/id/${photoId}/200/200`;
 // This URL can be used to get an array of objects that contain information
 // about various photos.
 const PHOTO_LIST_URL = "https://picsum.photos/list";
 
 class App extends Component {
-  // 1. Declare a state object that will be used to track an array of photos
-  state = {};
+    state = {
+        photos: [],
+    };
 
-  // 2. Declare a life cycle method
-  // This life cycle method should:
-  //  - will be called after the component is initially rendered
-  // - will fetch an array of photos
-  // - will add that array of photos to state once received
-  render() {
-    const { photos = [] } = this.state;
-    return (
-      <React.Fragment>
-        <header>
-          <h1>Photo Wall</h1>
-          <p>
-            Start by reading App.jsx and completing the numbered steps.
-            Afterward, delete this paragraph. Then, open up App.css and complete
-            the instructions there.
-          </p>
-        </header>
-        <div className="collage">
-          {/* We use map here because Array.prototype.map is an expression,
-           * and for loops are not. You'll learn more about this soon!
-           */}
-          {photos.map(photo => (
-            <img
-              alt={/* 3. Fill me in with the photo's filename */ ""}
-              key={/* 4. Fill me in with the photo's id */ ""}
-              src={/* 5. Fill me in with the photo's URL */ ""}
-            />
-          ))}
-        </div>
-      </React.Fragment>
-    );
-  }
+    componentDidMount = async () => {
+        try {
+            const photosArr = await (await fetch(PHOTO_LIST_URL)).json();
+            this.setState(() => ({
+                photos: photosArr,
+            }));
+        } catch (err) {
+            console.log(err);
+            console.log("U dun goofed, kid");
+        }
+    };
+
+    render() {
+        const { photos = [] } = this.state;
+        return (
+            <React.Fragment>
+                <header>
+                    <h1>Photo Wall</h1>
+                </header>
+                <div className="collage">
+                    {photos.map((photo) => (
+                        <img
+                            alt={photo.filename}
+                            key={photo.id}
+                            src={PHOTO_URL(photo.id)}
+                        />
+                    ))}
+                </div>
+            </React.Fragment>
+        );
+    }
 }
 
 export default App;
