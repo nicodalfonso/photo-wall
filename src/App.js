f1744c040c36095b189cf5fa369d3b2a5d02a1b6
Nico Alfonso <nicodalfonso@gmail.com>
2021-03-16 00:05:28 -0400

    Refactors App.js to use hooks instead of classes

--- src/App.js
+++ src/App.js
@@ -1,76 +1,67 @@
-import React, { Component } from "react";
+import React, { useState, useEffect, useReducer } from "react";
 import "./App.css";
+import { ACTIONS, initialState, photoReducer } from "./photoReducer";
 
-// This URL can be combined with an photo id to fetch an photo.
+// This URL can be combined with a photo id to fetch a photo.
 const PHOTO_URL = (photoId) => `https://picsum.photos/id/${photoId}/200/200`;
 // This URL can be used to get an array of objects that contain information
 // about various photos.
 const PHOTO_LIST_URL = "https://picsum.photos/list";
 
-class App extends Component {
-    state = {
-        photos: [],
-        visiblePhotos: [],
-        photosPerLoad: 10,
-        timesLoaded: 0,
+export const App = () => {
+  const [state, dispatch] = useReducer(photoReducer, initialState);
+  const [photosArr, setPhotosArr] = useState([]);
+
+  // Fetches photos on mount
+  useEffect(() => {
+    const fetchPhotos = async () => {
+      try {
+        const data = await (await fetch(PHOTO_LIST_URL)).json();
+        setPhotosArr(data);
+        dispatch({ type: ACTIONS.FETCHED_PHOTOS, payload: { visiblePhotos: toJSX(data, 0, state.photosPerLoad) } });
+      } catch (err) {
+        console.error(err);
+      }
     };
-
-    componentDidMount = async () => {
-        try {
-            const photosArr = await (await fetch(PHOTO_LIST_URL)).json();
-            this.setState((state) => ({
-                photos: photosArr,
-                visiblePhotos: this.toJSX(photosArr, 0, state.photosPerLoad),
-                timesLoaded: state.timesLoaded + 1,
-            }));
-        } catch (err) {
-            console.log(err);
-            console.log("U dun goofed, kid");
-        }
+    fetchPhotos();
+    return () => {
+      fetchPhotos();
     };
+  }, [state.photosPerLoad]);
 
-    toJSX = (photosArr, firstPhoto, lastPhoto) => {
-        return photosArr
-            .slice(firstPhoto, lastPhoto)
-            .map((photo) => (
-                <img
-                    alt={photo.filename}
-                    key={photo.id}
-                    src={PHOTO_URL(photo.id)}
-                />
-            ));
-    };
+  // returns JSX tags for each image in a givenlist of photos
+  const toJSX = (photosArr, firstPhoto, lastPhoto) => {
+    return photosArr.slice(firstPhoto, lastPhoto).map((photo) => <img alt={photo.filename} key={photo.id} src={PHOTO_URL(photo.id)} />);
+  };
 
-    loadMorePhotos = (nextBatch) => {
-        this.setState((state) => ({
-            visiblePhotos: state.visiblePhotos.concat(nextBatch),
-            timesLoaded: state.timesLoaded + 1,
-        }));
-    };
+  // Handles loading new batch of photos to state when user requests more
+  const loadMorePhotos = (nextBatch) => {
+    const timesLoaded = state.timesLoaded + 1;
+    const startIndex = timesLoaded * state.photosPerLoad;
+    const endIndex = startIndex + state.photosPerLoad;
+    const upcomingPhotos = toJSX(photosArr, startIndex, endIndex);
+    const visiblePhotos = state.visiblePhotos.concat(upcomingPhotos);
+    dispatch({
+      type: ACTIONS.LOAD_PHOTOS,
+      payload: {
+        timesLoaded,
+        startIndex,
+        endIndex,
+        upcomingPhotos,
+        visiblePhotos,
+      },
+    });
+  };
 
-    render() {
-        const {
-            photos = [],
-            visiblePhotos: visiblePhotos = [],
-            photosPerLoad,
-            timesLoaded,
-        } = this.state;
-        const firstPhoto = timesLoaded * photosPerLoad;
-        const lastPhoto = (timesLoaded + 1) * photosPerLoad;
-        const nextBatch = this.toJSX(photos, firstPhoto, lastPhoto);
-
-        return (
-            <React.Fragment>
-                <header>
-                    <h1>Photo Wall</h1>
-                </header>
-                <div className="collage">{visiblePhotos}</div>
-                <button onClick={() => this.loadMorePhotos(nextBatch)}>
-                    Show me what you got
-                </button>
-            </React.Fragment>
-        );
-    }
-}
+  return (
+    <>
+      <header>
+        <h1>Photo Wall</h1>
+      </header>
+      <div className="collage">{state.visiblePhotos}</div>
+      <button onClick={() => loadMorePhotos(state.upcomingPhotos)}>Show me what you got</button>
+    </>
+  );
+};
 
 export default App;
